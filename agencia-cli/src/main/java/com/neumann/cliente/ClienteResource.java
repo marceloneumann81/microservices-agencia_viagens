e1a907e27eea2928e40131d8a6e4b6ccecea355d
marceloneumann81 <marneumann@gmail.com>
2023-02-10 14:31:57 -0300

    Timeout, fallback e CircuitBreaker no serviço cliente

--- agencia-cli/src/main/java/com/neumann/cliente/ClienteResource.java
+++ agencia-cli/src/main/java/com/neumann/cliente/ClienteResource.java
@@ -1,5 +1,7 @@
 package com.neumann.cliente;
 
+import java.time.temporal.ChronoUnit;
+
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -7,6 +9,9 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 @Path("/cliente-cli")
@@ -19,6 +24,14 @@ public class ClienteResource {
     @GET
     @Path("findById")
     @Produces(MediaType.APPLICATION_JSON)
+    @Timeout(unit = ChronoUnit.SECONDS, value = 3)
+    @Fallback(fallbackMethod = "fallback")
+    @CircuitBreaker(
+        requestVolumeThreshold = 4,
+        failureRatio = .5,
+        delay = 6000,
+        successThreshold = 1       
+    )
     public Cliente findById(@QueryParam("id") long id){
         return clienteService.findById(id);
     }
@@ -30,5 +43,10 @@ public class ClienteResource {
         
         return clienteService.newCliente(cliente);
     }
+
+    private Cliente fallback(long id){
+        return Cliente.of(0, "");
+    }
+
     
 }
